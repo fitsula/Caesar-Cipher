07a71a1bd546a72c9a3f4217f419502bef699d9a
Main <my@mail>
2016-09-26 17:48:20 +0000

    caesar enc is done

--- enc.py
+++ enc.py
@@ -1,7 +1,7 @@
 alphabet = ('а', 'б', 'в', 'г', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
             'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ю', 'я', 'ь')
 
-text = open('enc.txt', encoding='utf8').read()
+text = open('text.txt', encoding='utf8').read()
 print(text)
 
 key = input('Введіть ключ: ')
@@ -10,47 +10,21 @@ print('Key = ' + key)
 length = len(alphabet)
 print('Довжина абетки: ' + str(length))
 
-i = 0
-print('character in text: ')
 
-original = []
-new = []
+def caesar():
+    new_text = ""
+    for character in text:
+        n = 0  # який номер елементу в alphabet
+        done = 0
+        for element in alphabet:
+            if element == character:
+                letter = (int(n) + int(key)) % int(length)
+                new_text += alphabet[letter]
+                done = 1
+            n += 1
 
-for character in text:
-    #print(text[i])
+        if done == 0:
+            new_text += character
+    return new_text
 
-    #print(str(i) + " element in alphabet")
-    n = 0
-    for element in alphabet:
-        if element == text[i]:
-            alphabet_number = n % length
-            #print('yes, element = ' + element + ' and text[i] = ' + text[i] + ' and = ' + str(n) + ' in alphabet - ' +\
-                  #alphabet[alphabet_number])
-            original.append(alphabet_number)
-        #print(element)
-        n += 1
-    i += 1
-
-for original_number in original:
-    #print('Original number: ')
-    #print(original_number)
-    #print('New number: ')
-    new_number = (int(original_number) - int(key)) % int(length)    # if dec - int(key), if enc + int(key)
-    new.append(new_number)
-
-print('Original: ', end='')
-print(original)
-print('New: ', end='')
-print(new)
-
-print('\n\n\n=====================')
-print('Original text: ')
-print(text, end='\n\n')
-print('Encrypted text: ')
-
-
-for characters in new:
-    print(alphabet[characters], end='')
-
-
-# string += character       # method for good future
+print(caesar())
