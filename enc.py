f11832f46e94e133254fb6e9e7c710d2fe83a9c2
Main <my@mail>
2016-09-27 13:03:17 +0000

    change order in alphabet and add encryption for capital letters

--- enc.py
+++ enc.py
@@ -1,5 +1,8 @@
 alphabet = ('а', 'б', 'в', 'г', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
-            'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ю', 'я', 'ь')
+            'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я')
+
+capital_alphabet = ('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І', 'Ї', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р',
+                    'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я')
 
 text = open('text.txt', encoding='utf8').read()
 print(text)
@@ -15,6 +18,7 @@ def caesar():
     new_text = ""
     for character in text:
         n = 0  # який номер елементу в alphabet
+        c = 0  # який номер елементу в capital_alphabet
         done = 0
         for element in alphabet:
             if element == character:
@@ -22,6 +26,14 @@ def caesar():
                 new_text += alphabet[letter]
                 done = 1
             n += 1
+
+        if done != 1:
+            for element in capital_alphabet:
+                if element == character:
+                    letter = (int(c) + int(key)) % int(length)
+                    new_text += capital_alphabet[letter]
+                    done = 1
+                c += 1
 
         if done == 0:
             new_text += character
